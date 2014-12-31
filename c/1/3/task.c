ae1d0f8af1108bbe43763de2a9f93de68b22d7e4
haisland0909 <awaawashima@gmail.com>
2014-12-31 16:15:26 +0900

    for visual

--- c/1/3/task.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("もしもし。\n\nこんにちわ。\nそれでは。\n");
-    
-    return 0;
-}
--- c/1/4/task.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int a = 3.11;
-    printf("intの3.11は%dになります。" , a);
-    
-    return 0;
-}
